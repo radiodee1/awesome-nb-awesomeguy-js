7dbf0abaec7cd306ac1287f99ec439a456e4a802
David Liebman <radiodee1@gmail.com>
2017-01-17 14:36:11 -0500

    game_start.js

---
+++ public_html/js/game_start.js
@@ -0,0 +1,12 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+$(document).ready( function() {
+    //alert("game start");
+    $("#page_content").html("<canvas id='my_canvas' width='256' height='192' style='border:1px solid #000000;' ></canvas>")
+
+}) 
