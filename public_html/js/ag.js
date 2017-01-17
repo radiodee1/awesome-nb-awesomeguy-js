b5d502f4f9ea226716bb11070ba07f61dd2e661f
David Liebman <radiodee1@gmail.com>
2017-01-17 16:58:26 -0500

    AG constants

---
+++ public_html/js/ag.js
@@ -0,0 +1,52 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+class AG {};
+    
+AG.UP = 38;
+AG.DOWN = 40;
+AG.LEFT = 37;
+AG.RIGHT = 39;
+
+AG.B_START = 5;
+AG.B_SPACE = 0;
+AG.B_LADDER = 444;
+AG.B_BLOCK = 442;
+AG.B_GOAL = 446;
+AG.B_KEY = 445; 
+AG.B_PRIZE =  447;
+AG.B_MONSTER = 443;
+AG.B_MARKER = 441; 
+AG.B_DEATH = 439 ;
+AG.B_ONEUP = 438 ;
+AG.B_BIBPRIZE = 440 ;
+AG.B_PLATFORM = 437 ;
+AG.B_INITIAL_GOAL = 500;
+ 
+AG.TILEMAP_WIDTH = 224;
+AG.TILEMAP_HEIGHT = 128;
+ 
+AG.TILE_WIDTH = 8;
+AG.TILE_HEIGHT = 8;
+ 
+AG.GUY_WIDTH = 16;
+AG.GUY_HEIGHT = 16;
+AG.GUY_CHEAT = 3;
+ 
+AG.MONSTER_WIDTH = 16;
+AG.MONSTER_HEIGHT = 16;
+ 
+AG.PLATFORM_WIDTH = 40;
+AG.PLATFORM_HEIGHT = 8;
+ 
+AG.MAP_WIDTH = 96;
+AG.MAP_HEIGHT = 96;
+ 
+AG.SCREEN_WIDTH = 256;
+AG.SCREEN_HEIGHT = 192;
+    
+    
