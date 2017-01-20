131f55221bc38482bcd73754b64ae07058820daa
David Liebman <radiodee1@gmail.com>
2017-01-20 17:54:28 -0500

    changes to test

--- public_html/js/ag_loop_test.js
+++ public_html/js/ag_loop_test.js
@@ -7,22 +7,21 @@
 
 
 $(document).ready( function() {
-    //$("#my_canvas").
-    
-    
+    guy.y = 132;
+    guy.x = 16;
+    guy.animate = 0;
     
     setupDrawFunctions();
     
-    guy.y = 32;
     //var screen = getScreenPointer(0);
     //screen.putImageData(platform_a, 0,0);
-    drawSprite_40_8(platform_a, 0,0,0,0, 0, 0);
-    drawSprite_16(guy_a,16,16,0,0, 0,0);
-    drawScoreWords();
-    alert(tiles_a);
-    var square = cutTile("tiles1", 0, 10);
+    //drawSprite_40_8(platform_a, 0,0,0,0, 0, 0);
+    //drawSprite_16(guy_a,16,16,0,0, 0,0);
+    //drawScoreWords();
+    
+    //var square = cutTile("tiles1", 0, 10);
     //alert(square);
-    drawTile_8(square, 0,0,0,0, 0,0)
+    //drawTile_8(square, 0,0,0,0, 0,0)
     
     
     //drawLevel(0);
