f1bde7f6644fb0ff3bdca17b4b5747a5ae177b5f
David Liebman <radiodee1@gmail.com>
2017-02-05 13:05:53 -0500

    settings

--- public_html/js/game_start.js
+++ public_html/js/game_start.js
@@ -12,31 +12,32 @@ var MOVE_CONST = 3;
 
 $(window).on( "load", function() {
     
-    setupDrawFunctionsA();
-    
-    $(document).keydown(function(e) {
-        input_key = e.keyCode;
-        //$("#page_footer").html("<p>tap " + input_key  + "</p>");
-        //// call game fn here
-        if (input_key === AG.LEFT) move_lr = - MOVE_CONST;//guy.x --;
-        if (input_key === AG.UP) move_ud = - MOVE_CONST; //guy.y --;
-        if (input_key === AG.RIGHT) move_lr = MOVE_CONST;// ++;
-        if (input_key === AG.DOWN) move_ud = MOVE_CONST;
-        if (input_key === AG.JUMP) move_jump = MOVE_CONST;
-        //drawLevel(0);
+    if (true ) {
+        setupDrawFunctionsA();
 
-    });
-    
-    $(document).keyup(function(e) {
-        input_key = 0;
-        //$("#page_footer").html("<p>tap " + input_key + "</p>");
-        //// call game fn again here
-        move_lr = 0;
-        move_ud = 0;
-        move_jump = 0;
-    });
-    //console.log("ready?");
-    
-    setupDrawFunctionsB();
-    
+        $(document).keydown(function(e) {
+            input_key = e.keyCode;
+            //$("#page_footer").html("<p>tap " + input_key  + "</p>");
+            //// call game fn here
+            if (input_key === AG.LEFT) move_lr = - MOVE_CONST;//guy.x --;
+            if (input_key === AG.UP) move_ud = - MOVE_CONST; //guy.y --;
+            if (input_key === AG.RIGHT) move_lr = MOVE_CONST;// ++;
+            if (input_key === AG.DOWN) move_ud = MOVE_CONST;
+            if (input_key === AG.JUMP) move_jump = MOVE_CONST;
+            //drawLevel(0);
+
+        });
+
+        $(document).keyup(function(e) {
+            input_key = 0;
+            //$("#page_footer").html("<p>tap " + input_key + "</p>");
+            //// call game fn again here
+            move_lr = 0;
+            move_ud = 0;
+            move_jump = 0;
+        });
+        //console.log("ready?");
+
+        setupDrawFunctionsB();
+    }
 });
