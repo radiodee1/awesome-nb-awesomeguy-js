4ac47f55864ef4e80779011152c31472d0730e33
David Liebman <radiodee1@gmail.com>
2025-07-04 09:24:43 -0400

    imageSmoothingQuality

--- public_html/js/ag_loop_test.js
+++ public_html/js/ag_loop_test.js
@@ -186,11 +186,8 @@ function testImageMag() {
     cctx.imageSmoothingQuality = 'high';
 
     image.onload = () => {
-        //cctx.drawImage(image, 0,0, test_pixels(100, 'width'), test_pixels(100, 'height'));
-        cctx.drawImage(image, 0,0, test_pixels(100, 'width'), test_pixels(100, 'height'),0,0, test_pixels(100, 'width'), test_pixels(100, 'height'));
-
-
-
+        cctx.drawImage(image, 0,0, test_pixels(100, 'width'), test_pixels(100, 'height'));
+        //cctx.drawImage(image, 0,0);
     }
 }
 
